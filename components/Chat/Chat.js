5ede0fd479f576b650775cfae5da1ae66c12c46d
Andypedozz <andypedini02@gmail.com>
2025-05-28 15:57:45 +0200

    .

--- components/Chat/Chat.js
+++ components/Chat/Chat.js
@@ -1,8 +1,9 @@
 
 class Chat {
     
-    constructor(containerId, type, messages) {
+    constructor(containerId, name, type, messages) {
         this.containerId = containerId;
+        this.name = name;
         this.messages = messages;
         this.type = type;
         this.loadChat();
@@ -11,6 +12,16 @@ class Chat {
 
     loadChat() {
         const container = document.getElementById(this.containerId);
+
+        const chatHeader = document.createElement("div");
+        chatHeader.setAttribute("class","chat-header");
+        const h3 = document.createElement("h3");
+        h3.innerText = this.name;
+        chatHeader.appendChild(h3);
+        container.appendChild(chatHeader);
+
+        const chatMessagesDiv = document.createElement("div");
+        chatMessagesDiv.setAttribute("class","chat-messages");
         
         this.messages.forEach(message => {
             const div = document.createElement("div");
@@ -23,16 +34,30 @@ class Chat {
             if(message.SentReceived) div.setAttribute("class","message sent");
             else div.setAttribute("class","message received");
             div.innerText = message.Text;
-            container.appendChild(div);
+            chatMessagesDiv.appendChild(div);
         });
+
+        container.appendChild(chatMessagesDiv);
 
     }
     
     loadTextBar() {
         const container = document.getElementById(this.containerId);
         const textBarDiv = document.createElement("div");
+        const span1 = document.createElement("span");
+        span1.innerText = "insert_emoticon";
+        span1.setAttribute("class","material-icons icon");
+        const input = document.createElement("input");
+        input.type = "text";
+        input.placeholder = "Scrivi un messaggio...";
+        const span2 = document.createElement("span");
+        span2.setAttribute("class","material-icons icon");
+        span2.innerText = "send";
         textBarDiv.setAttribute("class","textBarDiv");
 
+        textBarDiv.appendChild(span1);
+        textBarDiv.appendChild(input);
+        textBarDiv.appendChild(span2);
         container.appendChild(textBarDiv);
     }
 
